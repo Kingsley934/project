ff393c6fdcda0faf17503815e7b8d9d18d31386b
Kingsley934 <82475619+Kingsley934@users.noreply.github.com>
2021-04-16 20:26:05 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <!DOCTYPE html>
 <html>
-<img src="king.jpg" />
-<img src="babe.jpg" />
+
+<style type="text/css">
+#king {max-height:50px;
+	max-width: 50px;
+}
+#babe {} 
+
+<stysle>
+
+<div id="king"><img src="king.jpg" /></div>
+<div id="babe"><img src="babe.jpg" /></div>
+
 </html>
 
 <?php
