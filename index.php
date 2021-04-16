c9b914073ec4b141e150d6a31f97746b9ba7dafa
Kingsley934 <82475619+Kingsley934@users.noreply.github.com>
2021-04-16 20:34:53 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,21 +1,10 @@
 <!DOCTYPE html>
 <html>
 
-<style type="text/css">
-.king {max-height:50px;
-	max-width: 50px;
-}
-#babe {} 
-
-<stysle>
-
-<div class="king"><img src="king.jpg" /></div>
-<div id="babe"><img src="babe.jpg" /></div>
-
 </html>
 
 <?php
- echo "welcome Mr. Goliath";
+ echo "Welcome Mr. Goliath";
 
 ?>
 
