89559fbe115978d38fac17881b2dfd5559f7c2a3
Kingsley934 <82475619+Kingsley934@users.noreply.github.com>
2021-04-17 00:18:28 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
+<!DOCTYPE html>
 
-<?php
- echo "Hey <b>Pemphero</b>, I just want to let you know that your the lady of my dreams. i <b>love</b> you!";
-
-?>
+<html>
+<p>hie</p>
+<p>that side</p>
+</html>
 
