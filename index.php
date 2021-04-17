2b80209c21442495bca5da40938e3c6f56814f0d
Kingsley934 <82475619+Kingsley934@users.noreply.github.com>
2021-04-17 23:13:50 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html>
-<style type="text/css">
-p {font-size:20px;}
-</style>
-
 <html>
-<p>Hey, am sorry but I think I value you more than my ego and this is exactly the reason why I can't keep pretending everything is fine with me when I know am missing you every single moment.</p>
-<img src="xila.jpg" />
+<head>
+<style type="text/css">
+p {font-size:20px;
+   font-style:Verdana,Arial;}
+a:link {color:red;
+text-decoration:none;}
+a:active {color:green;}
+a:hover {background-color:blue;
+text-decoration:underline;
+color:white;
+font-weight:bold;}
+a:visited {color:green;}
+</style>
+<head>
+<body>
+<p>We all make mistakes and am one of those who always believe that am not always perfect. This is why I have humbled myself because my ego has no value but you. Am sorry for everything that happened the day we argued.</p>
+<p>People misunderstand each other but things should be talked over and life should keep moving.</p>
+<h3><a href="trial2.php">This message goes to you</a></h3>
+</body>
 </html>
-
