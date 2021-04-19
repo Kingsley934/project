3cb08fcf14faa1024111bc802ecd93aad7b149ec
Kingsley934 <82475619+Kingsley934@users.noreply.github.com>
2021-04-19 12:27:59 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,24 +1,35 @@
-<!DOCTYPE html>
-<html>
-<head>
-<style type="text/css">
-p {font-size:20px;
-   font-style:Verdana,Arial;}
-a:link {color:red;
-text-decoration:none;}
-a:active {color:green;}
-a:hover {background-color:blue;
-text-decoration:underline;
-color:white;
-font-weight:bold;}
-a:visited {color:green;}
-</style>
-<head>
-<body>
-<p>We all make mistakes and am one of those who always believe that am not always perfect. This is why I have humbled myself because my ego has no value but you. Am sorry for everything that happened the day we argued.</p>
-<p>People misunderstand each other but things should be talked over and life should keep moving.</p>
-<br/>
-<p>Click the link below to check the owner of the message...</p>
-<h3><a href="trial2.php">This message goes to you</a></h3>
-</body>
-</html>
+<?php
+// Read the variables sent via POST from our API
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text        = $_POST["text"];
+
+if ($text == "") {
+    // This is the first request. Note how we start the response with CON
+    $response  = "CON What would you want to check \n";
+    $response .= "1. My Account \n";
+    $response .= "2. My phone number";
+
+} else if ($text == "1") {
+    // Business logic for first level response
+    $response = "CON Choose account information you want to view \n";
+    $response .= "1. Account number \n";
+
+} else if ($text == "2") {
+    // Business logic for first level response
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your phone number is ".$phoneNumber;
+
+} else if($text == "1*1") { 
+    // This is a second level response where the user selected 1 in the first instance
+    $accountNumber  = "ACC1001";
+
+    // This is a terminal request. Note how we start the response with END
+    $response = "END Your account number is ".$accountNumber;
+
+}
+
+// Echo the response back to the API
+header('Content-type: text/plain');
+echo $response;
