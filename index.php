904412e16eab8c1ef2e1385732ac6b628debdfaf
Kingsley934 <82475619+Kingsley934@users.noreply.github.com>
2021-04-21 18:47:57 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ $text        = $_POST["text"];
 $firstname = "";
 $lastname  = "";
 $age       = "";
+$counter =0;
 
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
@@ -16,16 +17,19 @@ if ($text == "") {
 }else if ($text != "") {
     // Business logic for first level response
     $firstname = $text;
+    $counter = $counter + 1;
     $response = "CON Enter your lastname\n";
+    
 
-}else if ($firstname != "") {
+}else if ($firstname != "" && $counter ==1) {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
     $firstname_length = strlen($firstname)+1;
     $lastname = substr($text,$firstname_length);
+    $counter = $counter + 1;
     $response = "CON Enter your age";
 
-}else if($lastname != "") { 
+}else if($lastname != "" && $counter ==2) { 
     // This is a second level response where the user selected 1 in the first instance
     $lastname_length = strlen($lastname) + 1;
     $age = substr($text,$firstname_length+$lastname_length);
