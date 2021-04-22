e9268420662c227099c8f3623fa0c9199b0b841a
Kingsley934 <82475619+Kingsley934@users.noreply.github.com>
2021-04-22 21:58:36 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -4,40 +4,32 @@ $sessionId   = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
 $text        = $_POST["text"];
-$firstname = "";
-$lastname  = "";
-$age       = "";
-$counter =0;
 
-if ($text == "" && $counter ==0) {
+if ($text == "") {
     // This is the first request. Note how we start the response with CON
-    $counter = $counter + 1;
-    $response  = "CON Welcome to the MUST registration site \n";
-    $response.="Enter your firstname";
+    $response  = "CON What would you want to check \n";
+    $response .= "1. My Account \n";
+    $response .= "2. My phone number";
 
-}else if ($text != "" && $counter ==1) {
+} else if ($text == "1") {
     // Business logic for first level response
-    $firstname = $text;
-    $counter = $counter + 1;
-    $response = "CON Enter your lastname\n";
-    
+    $response = "CON Choose account information you want to view \n";
+    $response .= "1. Account number \n";
 
-}else if ($firstname != "" && $counter ==2) {
+} else if ($text == "2") {
     // Business logic for first level response
     // This is a terminal request. Note how we start the response with END
-    $firstname_length = strlen($firstname)+1;
-    $lastname = substr($text,$firstname_length);
-    $counter = $counter + 1;
-    $response = "CON Enter your age";
+    $response = "END Your phone number is ".$phoneNumber;
 
-}else if($lastname != "" && $counter ==3) { 
+} else if($text == "1*1") { 
     // This is a second level response where the user selected 1 in the first instance
-    $lastname_length = strlen($lastname) + 1;
-    $age = substr($text,$firstname_length+$lastname_length);
+    $accountNumber  = "ACC1001";
 
     // This is a terminal request. Note how we start the response with END
-    $response = "END $firstname $lastname is aged $age";
+    $response = "END Your account number is ".$accountNumber;
 
 }
+
+// Echo the response back to the API
 header('Content-type: text/plain');
 echo $response;
