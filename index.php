661e19a3299f4feb3d0aa37864e68ca0d131000a
Kingsley934 <82475619+Kingsley934@users.noreply.github.com>
2021-04-23 23:11:48 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -5,39 +5,17 @@ $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
 $text        = $_POST["text"];
 $firstname = "";
-$lastname  = "";
-$age       = "";
-$counter =0;
 
 if ($text == "") {
     // This is the first request. Note how we start the response with CON
-    $counter = $counter +1;
     $response  = "CON Welcome to the MUST registration site \n";
     $response.="Enter your firstname";
 
-}else if ($counter ==1) {
+}else if ($text != "") {
     // Business logic for first level response
     $firstname = $text;
-    $counter = $counter + 1;
-    $response = "CON Enter your lastname\n";
-    
-
-}else if ($counter ==2) {
-    // Business logic for first level response
-    // This is a terminal request. Note how we start the response with END
-    $firstname_length = strlen($firstname)+1;
-    $lastname = substr($text,$firstname_length);
-    $counter = $counter + 1;
-    $response = "CON Enter your age";
-
-}else if($counter ==3) { 
-    // This is a second level response where the user selected 1 in the first instance
-    $lastname_length = strlen($lastname) + 1;
-    $age = substr($text,$firstname_length+$lastname_length);
-
-    // This is a terminal request. Note how we start the response with END
-    $response = "END $firstname $lastname is aged $age";
-
+    $response = "END Your name is $firstname\n";
+    $response = "Thank You for Using our system";
 }
 header('Content-type: text/plain');
 echo $response;
