697b2d37649cd8d3811f99e1ce324b6331a5c9cf
Kingsley934 <82475619+Kingsley934@users.noreply.github.com>
2021-04-29 21:36:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,40 +3,34 @@
 $sessionId   = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
-$counter = 0;
 $text        = $_POST["text"];
 
 
 if ($text == "") {
 	// This is the first request. Note how we start the response with CON
-    	$response  = "CON TONSE Training Programmes \n";
-    	$response  .= "Welcome, please provide accurate information\n";
-    	$response.="Choose your title below: \n";
-    	$response .="1. Mr\n";
-    	$response .="2. Mrs\n";
-    	$response .="3. Miss";
+    	$response    = "CON TONSEFE Training Programmes \n";
+    	$response   .= "Welcome, please provide accurate information\n";
+    	$response   .= "Choose your title below: \n";
+    	$response   .= "1. Mr\n";
+    	$response   .= "2. Mrs\n";
+    	$response   .= "3. Miss";
+	
 }else if ($text != "") {
-	if($GLOBALS['counter'] == 0){
+	if(strlen($text) == 1){
 		$checker = $text;
 		if($checker == "1"){
 			$GLOBALS['title'] ="Mr.";
-			$counter = $counter +1;
 		}else if($checker =="2"){
 			$GLOBALS['title'] = "Mrs.";
-			$counter = $counter +1;
 		}else if($checker =="3"){
 			$GLOBALS['title'] = "Miss";
-			$counter = $counter +1;
 		}else{
 			$response ="END invalid input, please try again...!";
 		}
 		$GLOBALS['title_length'] = strlen($GLOBALS['title'];
-	}else if($GLOBALS['counter'] == 1){
+	}else if(isset($GLOBALS['title'])){
 		$response = "END thank you ".$GLOBALS['title']." ".$GLOBALS['title_length'];
-	}
-	
-	
-	
+	}	
 }
 header('Content-type: text/plain');
 echo $response;
