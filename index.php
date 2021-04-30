b040e0048cfd4cf90274d907967bc65ed02fad48
Kingsley934 <82475619+Kingsley934@users.noreply.github.com>
2021-04-30 23:32:52 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,44 +1,33 @@
 <?php
-
-
 // Read the variables sent via POST from our API
 $sessionId   = $_POST["sessionId"];
 $serviceCode = $_POST["serviceCode"];
 $phoneNumber = $_POST["phoneNumber"];
-$title = null;
-$name  = null;
 $text        = $_POST["text"];
 
+$level = explode("*",$text);
 
-if ($text == "") {
-	// This is the first request. Note how we start the response with CON
-    	$response    = "CON TONSEFE Training ProgrammeS \n";
-    	$response   .= "Welcome, please provide accurate information\n";
-    	$response   .= "Choose your title below: \n";
-    	$response   .= "1. Mr\n";
-    	$response   .= "2. Mrs\n";
-    	$response   .= "3. Miss";
-	
-}else if($text != ""){
-	if(strlen($text) == 1){
-		$checker = $text;
-		if($checker == "1"){
-			$GLOBALS['title'] = "Mr.";
-			$response = "CON Enter the age of the student";
-		}else if($checker == "2"){
-			$GLOBALS['title'] = "Mrs.";
-			$response = "CON Enter the age of the student";
-		}else if($checker == "3"){
-			$GLOBALS['title'] = "Miss";
-			$response = "CON Enter the age of the student";
-		}else{
-			$response = "END Invalid input entered...";
-		}
-	}else if(strlen($text) > 1){
-		$response = "END Thank you for using our system ".$GLOBALS['title']; 
-	
+if(isset($text)){
+	if($text == ""){
+		$response = "CON Welcome to the registration portal.\nPlease enter your full name";
 	}
-
-}
+	
+	if(isset($level[0]) && $level[0] !="" && !isset($level[1])){
+		$response = "CON Hi ".$level[0].", enter your ward name";
+	}else if(isset($level[1]) && $level[1] !="" && !isset($level[2])){
+		$response = "CON Please enter your national ID number";
+	}else if(isset($level[2]) && $level[2] !="" && !isset($level[3])){
+		$data = array(
+			'phonenumber'=>$phoneNumber, 
+			'fullname'=>$level[1], 
+			'electoral_ward' =>$level[2],
+			'national_id'=>$level[3]
+		);
+		
+		$response = "END Thank you ".$level[1]." for registering.\nWe will keep you updated";
+	}
+	
 header('Content-type: text/plain');
 echo $response;
+}
+?>
