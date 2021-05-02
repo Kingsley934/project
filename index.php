3583aa076ba85f43e19af57f7d15289b3031bed9
Kingsley934 <82475619+Kingsley934@users.noreply.github.com>
2021-05-02 12:32:13 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,34 +1,91 @@
-<!DOCTYPE html>
-<html>
-<head>
-<style type = "text/css">
+<?php
+// Read the variables sent via POST from our API
+$sessionId   = $_POST["sessionId"];
+$serviceCode = $_POST["serviceCode"];
+$phoneNumber = $_POST["phoneNumber"];
+$text        = $_POST["text"];
 
-	body {font-size:20px;}
-	a:link     {color:red;
-		        text-decoration:underline;}
-	a:visited  {color:green;}
-	a:active   {background-color:orange;}
-	a:hover    {background-color:blue;
-		        color:white;
-			    text-decoration:none;
-			    font-weight:bold;}
-			
-			
+$level = explode("*",$text);
+
+if(isset($text)){
+	if($text == ""){
+		$response = "CON TONSEFE Training Programmes \nProvide us with accurate information\n\n Choose your title below";
+		$response .= "1. Mr\n";
+		$response .= "2. Mrs\n";
+		$response .= "3. Miss\n";
+	}
 	
-	#fifth{background-color:red;
-		   color:white;}
+	if(isset($level[0]) && $level[0] !="" && !isset($level[1])){ 
+		if($level[0] != "1" && $level[0] != "2" && $level[0] != "3"){
+			$response = "END Invalid input. Please try Again!";
+		
+		}else{
+			$response = "CON Enter your First Name: ";
+		
+		}
+		
+	}else if(isset($level[1]) && $level[1] !="" && !isset($level[2])){
+		$level[1] = trim($level[1]);
+		$level[1] = stripslashes($level[1]);
+		$level[1] = strtolower($level[1]);
+		$level[1] = ucfirst($level[1]);
+		if(!preg_match("/^[a-zA-Z ]*$/",$level[1])){
+			$response = "END Invalid input. Please try Again!";
+		}else{
+			$response = "CON Enter your Last Name: ";
+		}
+		
+	}else if(isset($level[2]) && $level[2] !="" && !isset($level[3])){
+		$level[2] = trim($level[2]);
+		$level[2] = stripslashes($level[2]);
+		$level[2] = strtolower($level[2]);
+		$level[2] = ucfirst($level[2]);
+		if(!preg_match("/^[a-zA-Z ]*$/",$level[2])){
+			$response = "END Invalid input. Please try Again!";
+		}else{
+			$response = "CON Enter your Centre Name: ";
+		}
+		
+		
+	}else if(isset($level[3]) && $level[3] !="" && !isset($level[4])){
+		$level[3] = trim($level[3]);
+		$level[3] = stripslashes($level[3]);
+		$level[3] = strtolower($level[3]);
+		$level[3] = ucfirst($level[3]);
+		if(!preg_match("/^[a-zA-Z ]*$/",$level[3])){
+			$response = "END Invalid input. Please try Again!";
+		}else{
+			$response = "CON What is the number of female children that attended the program today: ";
+		}
+		
+		
+	}else if(isset($level[4]) && $level[4] !="" && !isset($level[5])){
+		if(is_numeric($level[4])){
+			$response = "CON What is the number of male children that attended the program today: ";
+		}else{
+			$response = "END Invalid input. Please try Again!";
+		}
+		
+		
+	}else if(isset($level[5]) && $level[5] !="" && !isset($level[6])){
+		if(is_numeric($level[5])){
+			$response = "CON Enter total number of teachers that attended the program today: ";
+		}else{
+			$response = "END Invalid input. Please try Again!";
+		}
+		
+		
+	}else if(isset($level[6]) && $level[6] !="" && !isset($level[7])){
+		if(is_numeric($level[6])){
+			$response = "CON Data Successfully Submitted!\n Thank you ".$level[0]." ".$level[2];
+		}else{
+			$response = "END Invalid input. Please try Again!";
+		}
+		
+		
+	}
 	
-			
-</style>
-</head>
-
-<body>
-<div id="first"><p>On a day like this, the world gave me a special person in the name of love. She's very lovely, elegant, adorable and gorgeous.</p></div>
-<div id="second"><p>She's my rose flower and guess what, her name is Rose too</p></div>
-<div id="third"><p>I don't think I can find the right words to describe how important and how good you are in my life because i don't think there would be such words </p></div>
-<div id="fourth"><p>Distance has kept us apart on this special day, just know that am always with you everyday,everytime for you are the only person I think about.</p></div>
-<div id="fifth"><p>Am so happy for you on this day, happy birthday my babe</p></div>
-<div id="sixth"><a href="second.php">Check link below</a></div>
-</body>
-
-</html>
+header('Content-type: text/plain');
+echo $response;
+}
+?>
